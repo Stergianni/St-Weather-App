7cd6eecb77c4b9010ee1b126d25330eb39cc173c
Stergianni <stella.orfanidis@hotmail.com>
2025-04-22 10:41:47 +0200

    chore: deploy static build to GitHub Page

--- weather-app/next.config.mjs
+++ weather-app/next.config.mjs
@@ -1,13 +1,10 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'docs',
-  basePath: '/St-Weather-App',
   images: {
-    unoptimized: true,
     domains: ['openweathermap.org'],
   },
+  output: 'export',
+  distDir: 'docs',
 };
 
 export default nextConfig;
